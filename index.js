50e16208888ffcac1d39c35d3b702a46510c0b9a
Aadil Ahmed <aadilahmed0@gmail.com>
2023-04-21 01:27:55 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(function(object) {
+      const node = document.createElement("li");
+      const textnode = document.createTextNode(object.id);
+      node.appendChild(textnode);
+      document.body.appendChild(node);
+    })
+    .catch(function(error) {
+      const node = document.createElement("li");
+      const textnode = document.createTextNode(error);
+      node.appendChild(textnode);
+      document.body.appendChild(node);
+    })
+} 
